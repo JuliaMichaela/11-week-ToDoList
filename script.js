bea9145362cf2595192b981866b96a10c780326c
JuliaMichaela <juliaperez@yandex.ru>
2024-10-18 16:47:44 +0300

    created function createTask ()

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function addTask {
-    const 
+function createTask () {
+    const task = document.getElementById('task');
+    const taskContent = task.textContent;
+
+    const list = document.getElementById('contentList')
+    const listItem = document.createElement('li');
+    listItem.innerText = task.value;
+    list.append(listItem);
+
+
+
+    
+    console.log (taskContent);
+    
+    console.log (result)
 }
+// console.log ()
