34282910d8e07f94352fc7260c263f23acebf95e
JuliaMichaela <juliaperez@yandex.ru>
2024-10-19 23:07:37 +0300

    updated js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function createTask () {
     const task = document.getElementById('task');
-    const taskContent = task.textContent;
-
     const list = document.getElementById('contentList')
     const listItem = document.createElement('li');
     listItem.innerText = task.value;
     list.append(listItem);
+    task.value = '';
+}
+
+function checkTask (){
+
+
+    
 }
